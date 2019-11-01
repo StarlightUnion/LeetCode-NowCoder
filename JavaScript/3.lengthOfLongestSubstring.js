7c98cce74311bacedc7576663700083c22d6c51d
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-11-01 19:16:06 +0800

    2019年11月01日 19:14:11

--- JavaScript/3.lengthOfLongestSubstring.js
+++ JavaScript/3.lengthOfLongestSubstring.js
@@ -1,4 +1,5 @@
 // Created by wxc on 2019/10/31
+// Updated on 2019/11/01
 
 // 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
@@ -28,7 +29,28 @@ var lengthOfLongestSubstring = function(s) {
     return len;
 };
 
-let str = "";
-console.log(lengthOfLongestSubstring(str));
+console.log(lengthOfLongestSubstring(""));
 
 // 题解中的 滑动窗口
+// time: 80ms memory: 37.3MB O(n)
+var lengthOfLongestSubstring = function(s) {
+    if(s.length < 1) {
+        return 0;
+    }
+
+    let len = 0, left = 0, right = 1;
+    while(right < s.length) {
+        let str = s.slice(left, right);
+        let flag = str.indexOf(s[right]);
+        if(flag >= 0) {
+            left += flag + 1;
+        } else {
+            len = right - left + 1 > len ? right - left + 1 : len;
+        }
+
+        right++;
+    }
+    return len;
+}
+
+console.log(lengthOfLongestSubstring("abcbacbb"));
