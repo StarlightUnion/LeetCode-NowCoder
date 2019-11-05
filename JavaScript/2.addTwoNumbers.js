ba71a52e306333a406904e5fb86b279f69cde7d0
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-11-05 21:40:40 +0800

    2019年11月05日 21:40:22

--- JavaScript/2.addTwoNumbers.js
+++ JavaScript/2.addTwoNumbers.js
@@ -1,4 +1,5 @@
 // Created by wxc on 2019/10/31
+// Updated on 2019/11/05
 
 // 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
 // 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。您可以假设除了数字 0 之外，这两个数都不会以 0 开头
@@ -14,12 +15,39 @@ function ListNode(val) {
     this.next = null;
 }
 
+// time: 140ms memory: 38.2MB O()
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    let res = new ListNode(null);
+    let nres = res;
+    let carry = 0, val = 0;// 进位 和 值
+    let v1 = 0, v2 = 0;// 每个链表的值
+
+    while(l1 || l2) {
+        v1 = l1 ? l1.val : 0;
+        v2 = l2 ? l2.val : 0;
+
+        val = (v1 + v2 + carry) % 10;
+        carry = Math.floor((v1 + v2 + carry) / 10);
+
+        nres.next = new ListNode(val);
+        nres = nres.next;
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    if(carry) {
+        nres.next = new ListNode(carry);
+    }
+
+    return res.next;
 };
 
+let l1 = {val: 2, next: {val: 4, next: {val: 3, next: null}}};
+let l2 = {val: 5, next: {val: 6, next: {val: 4, next: null}}};
+console.log(addTwoNumbers(l1, l2));
---
+++ JavaScript/4.findMedianSortedArrays.js
@@ -0,0 +1,28 @@
+// Created by wxc on 2019/11/04
+// Updated on 2019/11/05
+
+// 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+// 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+// 你可以假设 nums1 和 nums2 不会同时为空。
+// 示例1
+// nums1 = [1, 3]
+// nums2 = [2]
+// 则中位数是 2.0
+// 示例2
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+// 则中位数是 (2 + 3)/2 = 2.5
+
+// time: 144ms memory: 39.8MB
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = [...nums1, ...nums2].sort((a, b) => a - b);
+    let {length} = arr;
+    return length % 2 ? arr[Math.floor(length / 2)] : (arr[length / 2] + arr[length / 2 - 1]) / 2;
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
