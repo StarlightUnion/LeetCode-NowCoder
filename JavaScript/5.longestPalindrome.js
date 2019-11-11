cfff694e8e5a4f76bb2d72d8df14aaff10cb8996
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-11-11 17:08:29 +0800

    2019年11月11日 17:08:23

---
+++ JavaScript/5.longestPalindrome.js
@@ -0,0 +1,20 @@
+// Created by wxc on 2019/11/07
+
+// 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+// 示例1
+// 输入: "babad"
+// 输出: "bab"
+// 注意: "aba" 也是一个有效答案。
+// 示例2
+// 输入: "cbbd"
+// 输出: "bb"
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+};
+
+console.log(longestPalindrome("babad"));
