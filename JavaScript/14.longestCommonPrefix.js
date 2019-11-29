bb6c65f79f13e03e46723fa411b33e52fe9c6187
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-11-29 17:39:38 +0800

    2019年11月29日 17:39:20

--- JavaScript/14.longestCommonPrefix.js
+++ JavaScript/14.longestCommonPrefix.js
@@ -1,4 +1,5 @@
 // Created by wxc on 2019/11/27
+// Updated on 2019/11/29
 
 // 编写一个函数来查找字符串数组中的最长公共前缀。
 // 如果不存在公共前缀，返回空字符串 ""。
@@ -14,15 +15,33 @@
 // 说明:
 // 所有输入只包含小写字母 a-z 。
 
-//
+// 暴力法
+// 超出时间限制
 /**
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let index = 0;
+    let num = 1;// 个数
+    let flag = true;// 是否完成标志
     let matchStr = '';
 
+    if (strs.length) {
+        while(flag) {
+            matchStr = strs[0].substr(0, num);
+
+            for (let i = 0; i < strs.length; i++) {
+                flag = strs[i].startsWith(matchStr);
+            }
+            num++;
+        }
+    }
+
+    return matchStr.substr(0, matchStr.length - 1);
 };
+
+var longestCommonPrefix = function(strs) {
+
+}
 
 console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
