0c69beccba9d8e063dfeb464eed0ed2733dbd489
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-12-02 19:48:58 +0800

    2019年12月02日 19:48:27

--- JavaScript/10.isMatch.js
+++ JavaScript/10.isMatch.js
@@ -1,5 +1,5 @@
 // Created by wxc on 2019/11/25
-// Updated on 2019/11/26
+// Updated on 2019/12/02
 
 // 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
 // '.' 匹配任意单个字符
@@ -51,9 +51,57 @@ var isMatch = function(s, p) {
     return new RegExp(['^', ...p, '$'].join('')).test(s);
 };
 
-//
+// 动态规划法 LeetCode@秦时明月
+// time: 68ms memory: 35.9MB O(n^2)
+// 图解：https://leetcode-cn.com/problems/regular-expression-matching/solution/dong-tai-gui-hua-ji-hui-su-by-song-19/
 var isMatch = function(s, p) {
-    
+    if (s === null || p === null) {
+        return false;
+    }
+    var m = s.length + 1;
+    var n = p.length + 1;
+    // 初始化二维状态数组 && 初始化第i行第0列数据
+    var dp = new Array(m);
+    for (var i = 0; i < m; i++) {
+        dp[i] = new Array(n)
+        for (var j = 0; j < n; j++) {
+            dp[i][j] = false;
+        }
+    }
+    // 初始化第一个状态
+    dp[0][0] = true;
+    // 初始化第0行j列数据
+    for (var j = 2; j < n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 2];
+        }
+    }
+    // 动态转移表法
+    var curS = 0;//当前待匹配字符串i字符位置
+    var curP = 0;//当前匹配规则字符串j字符位置
+    var preCurP = 0;//当前匹配规则字符串j字符前一个位置
+    for (var i = 1; i < m; i++) {
+        for (var j = 1; j < n; j++) {
+            // 从1开始遍历
+            curS = s[i - 1];
+            curP = p[j - 1];
+            preCurP = p[j - 2];
+            if (curP === '.' || curS === curP) {
+                // 合并<1>、<2>步
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (curP === '*') {
+                // <5>步两重限制下只有一种情况【preCurP== '*'时不存在，且会报非法情况报语法错误 】
+                if(preCurP != '.'  && preCurP != curS){
+                    dp[i][j] = dp[i][j - 2]
+                }
+                // 合并<3><4><6><7><8>步，因为不在上述情况内
+                else {
+                    dp[i][j] = dp[i - 1][j - 2] || dp[i - 1][j] || dp[i][j - 2]
+                }
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
 }
 
 console.log(isMatch('aab', 'c*a*b'))
--- JavaScript/5.longestPalindrome.js
+++ JavaScript/5.longestPalindrome.js
@@ -1,5 +1,5 @@
 // Created by wxc on 2019/11/07
-// Updated on 2019/11/14
+// Updated on 2019/12/02
 
 // 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 // 示例1
@@ -10,12 +10,93 @@
 // 输入: "cbbd"
 // 输出: "bb"
 
+// 暴力法破解 滑动窗口
+// time: 192ms(54.93%) memory: 37.4MB(46.79%) O(n^2)
 /**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function(s) {
+    if (s.length <= 1) return s;
+    let res = '', _res = '';
 
+    // 1.aba模式
+    for (let i = 1; i < s.length - 1; i++) {
+        let flag = true;
+        let left = i - 1, right = i + 1;// 左右边界
+
+        while(flag) {
+            if (left >= 0 && right <= s.length - 1) {
+                if (s[left] === s[right]) {
+                    let str = s.slice(left, right + 1);
+
+                    res = str.length >= res.length ? str : res;
+                    left--;
+                    right++;
+                } else {
+                    flag = false;
+                }
+            } else {
+                flag = false;
+            }
+        }
+    }
+
+    // 2.baab模式
+    for (let i = 0; i < s.length; i++) {
+        let _flag = true;
+        let _left = i, _right = i + 1;// 左右边界
+
+        while(_flag) {
+            if (_left >= 0 && _right <= s.length - 1) {
+                if (s[_left] === s[_right]) {
+                    let _str = s.slice(_left, _right + 1);
+
+                    _res = _str.length >= _res.length ? _str : _res;
+                    _left--;
+                    _right++;
+                } else {
+                    _flag = false;
+                }
+            } else {
+                _flag = false;
+            }
+        }
+    }
+
+    if (!_res.length) _res = s[0];
+    return res.length > _res.length ? res : _res;
 };
 
-console.log(longestPalindrome("bc"));
+// 动态规划
+var longestPalindrome = function(s) {
+    if (s === "") {
+        return "";
+    }
+    let cell = [], maxLen = 0, maxEnd = 0;
+    for (let i = 0; i < s.length; i++) {
+        cell.push([]);
+        for (let j = 0; j < s.length; j++) {
+            let beforeIndex = s.length - 1 - i;
+            if (s[beforeIndex] === s[j]) {
+                if (i === 0 || j === 0) {
+                    cell[i][j] = 1;
+                } else {
+                    cell[i][j] = cell[i - 1][j - 1] + 1;
+                }
+            } else {
+                cell[i][j] = 0; // 不能省略
+            }
+            if (cell[i][j] > maxLen) {
+                let firstIndex = j - cell[i][j] + 1;
+                if (beforeIndex === firstIndex) {
+                    maxLen = cell[i][j];
+                    maxEnd = j;
+                }
+            }
+        }
+    }
+    return s.slice(maxEnd + 1 - maxLen, maxEnd + 1)
+}
+
+console.log(longestPalindrome("babad"));
