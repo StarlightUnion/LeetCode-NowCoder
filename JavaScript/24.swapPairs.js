37f9a72d947939ea0241d5fd40a05324c6184ebe
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-12-14 23:23:26 +0800

    2019年12月14日 23:23:19

--- JavaScript/24.swapPairs.js
+++ JavaScript/24.swapPairs.js
@@ -1,4 +1,5 @@
 // Created by wxc on 2019/12/06
+// Updated on 2019/12/14
 
 // 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
 // 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
@@ -48,6 +49,25 @@ var swapPairs = function(head) {
     }
 
     return res.next;
+};
+
+// 自递归 一 LeetCode@Nodreame
+// time: 60ms(94.8%) memory: 33.6MB(63.6%)
+// https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/jsliang-chong-jie-fa-die-dai-zi-di-gui-by-nodreame/
+var swapPairs = function (head) {
+    if (!head || !head.next) return head;
+    let next = head.next;
+    head.next = swapPairs(next.next);
+    next.next = head;
+    return next;
+};
+
+// 自递归 二 LeetCode@Nodreame
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    let [fst, snd] = [head, head.next];
+    [fst.next, snd.next] = [swapPairs(snd.next), fst];
+    return snd;
 };
 
 console.log(swapPairs({val: 1, next: {val: 2, next: {val: 3, next: {val: 4, next: null}}}}));
