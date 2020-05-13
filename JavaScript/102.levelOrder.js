c0ec6686f0864e66afd68aaef0b6ecc962987721
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2020-05-13 17:37:24 +0800

    2020年05月13日 17:37:19

---
+++ JavaScript/102.levelOrder.js
@@ -0,0 +1,35 @@
+// Created by wxc on 2020/05/13
+
+// 102. 二叉树的层序遍历
+// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+// 示例：
+// 二叉树：[3,9,20,null,null,15,7],
+// 3
+// / \
+// 9  20
+//  /  \
+// 15   7
+// 返回其层次遍历结果：
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+
+};
