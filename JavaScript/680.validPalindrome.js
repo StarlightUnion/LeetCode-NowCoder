13c8c19c62544f4978b0440c8356860043037baa
wangxingchen <2387574095@qq.com>
2020-07-02 22:00:36 +0800

    modify: update 680 2020/07/02 22:00:09

--- JavaScript/680.validPalindrome.js
+++ JavaScript/680.validPalindrome.js
@@ -18,11 +18,18 @@
 // 链接：https://leetcode-cn.com/problems/valid-palindrome-ii
 
 
-
+// LeetCode@jsyt
+// https://leetcode-cn.com/problems/valid-palindrome-ii/solution/js-4-xing-dai-ma-jian-dan-yi-dong-by-jsyt/
 /**
  * @param {string} s
  * @return {boolean}
  */
-var validPalindrome = function(s) {
-
+var validPalindrome = function(s, flag = true) {
+  let l = 0, r = s.length - 1;
+  while (l < r && s[l] === s[r]) l++, r--;
+  if (r <= l) return true;
+  if (flag == true) return validPalindrome(s.slice(l, r), false) || validPalindrome(s.slice(l + 1, r + 1), false)
+  return false;
 };
+
+console.log(validPalindrome("abca"));
